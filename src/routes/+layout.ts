526a576b33790d541555aae8dffb1e550bd80a19
Gerald <code@gera2ld.space>
2023-02-04 04:02:43 +0800

    fix: inject global css rather than preflight

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
-import { css, install } from '@twind/core';
+import { injectGlobal, install } from '@twind/core';
 import presetAutoprefix from '@twind/preset-autoprefix';
 import presetTailwind from '@twind/preset-tailwind';
 import { browser } from '$app/environment';
@@ -8,25 +8,25 @@ export const prerender = true;
 if (browser) {
 	install({
 		presets: [presetAutoprefix(), presetTailwind()],
-		preflight: css`
-			body {
-				@apply bg(white dark:zinc-800) text-zinc(800 dark:300);
-			}
-			a {
-				@apply text-blue(400 hover:600);
-			}
-			button {
-				@apply bg-transparent text-blue(400 hover:600);
-			}
-			select {
-				@apply bg-transparent border-b border-zinc-300;
-			}
-			input {
-				@apply bg-transparent border-b border-zinc-300 text-inherit text-xs;
-			}
-			.iconify {
-				@apply inline-block;
-			}
-		`,
 	});
+	injectGlobal`
+		body {
+			@apply bg(white dark:zinc-800) text-zinc(800 dark:300);
+		}
+		a {
+			@apply text-blue(400 hover:600);
+		}
+		button {
+			@apply bg-transparent text-blue(400 hover:600);
+		}
+		select {
+			@apply bg-transparent border-b border-zinc-300;
+		}
+		input {
+			@apply bg-transparent border-b border-zinc-300 text-inherit text-xs;
+		}
+		.iconify {
+			@apply inline-block;
+		}
+	`;
 }
