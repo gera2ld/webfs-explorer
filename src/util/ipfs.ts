871d2843bbd5302a21cc208fcc2d61cd13a345fb
Gerald <code@gera2ld.space>
2023-04-05 06:59:42 +0000

    fix: remove useless code

--- src/util/ipfs.ts
+++ src/util/ipfs.ts
@@ -8,13 +8,13 @@ async function createIpfsOnce() {
 		'https://cdn.jsdelivr.net/combine/npm/ipfs-core@0.18.0/dist/index.min.js,npm/@multiformats/multiaddr@11.4.0/dist/index.min.js'
 	);
 	const { create } = (await import('ipfs-core')).default;
-	const { multiaddr } = (await import('@multiformats/multiaddr')).default;
+	// const { multiaddr } = (await import('@multiformats/multiaddr')).default;
 	const ipfs: IPFS = await create();
-	ipfs.swarm.connect(
-		multiaddr(
-			'/dns4/swarm.ipfs.gerald.win/tcp/443/wss/p2p/12D3KooWDefwokBAv16Z9CpB5fCxkxa481xB5VHYSesmh4kkFchi'
-		)
-	);
+	// ipfs.swarm.connect(
+	// 	multiaddr(
+	// 		'/dns4/swarm.ipfs.gerald.win/tcp/443/wss/p2p/12D3KooWDefwokBAv16Z9CpB5fCxkxa481xB5VHYSesmh4kkFchi'
+	// 	)
+	// );
 	return ipfs;
 }
 
